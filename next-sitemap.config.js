9d318b366265b00b5770428776717b68fc995140
Jannick Pedersen <jannickpedersen@live.dk>
2025-02-17 20:39:28 +0100

    Minor add-ons

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://yourdomain.com',
+    siteUrl: 'https://neubrutalism-temp.vercel.app/',
     
     // Generate a robots.txt file alongside the sitemap(s)
     generateRobotsTxt: true,
